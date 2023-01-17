ee084b05a455809ac4c5d07dcd0fccf5fba6e901
Minerva Dutta <dutta.minerva@gmail.com>
2023-01-17 14:01:33 -0800

    changed case in the landing page

--- src/App.js
+++ src/App.js
@@ -8,14 +8,14 @@ import Shoes from './Components/Shoes';
 import Collections from './Components/Collections';
 import Template from './Components/Template';
 import {BrowserRouter as Router, Route, Routes,Link} from 'react-router-dom';
-// hi
+
 function App() {
   return (
     <>
     <Router>
         <Routes>
           <Route path ="/" element = {<Template />}>
-            <Route path ="Landing" element = {<><Bestsellers /><Collections/><Shirts /><Shoes /></>}/>
+            <Route path ="landing" element = {<><Bestsellers /><Collections/><Shirts /><Shoes /></>}/>
             <Route path ="women" element = {<Shirts />}/>
             <Route path ="kids" element = {<Shoes />}/>
             <Route path ="accessories" element = {<Bestsellers />}/>
